30fa7a6e516e844f6ff7712a056ba366f4df03c8
Sameera Viraj <sameera.emedia@gmail.com>
2020-06-03 02:24:25 +0530

    set data class

--- src/Form/Type/ProductType.php
+++ src/Form/Type/ProductType.php
@@ -2,12 +2,14 @@
 
 namespace App\Form\Type;
 
+use App\Entity\Product;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ProductType extends AbstractType
 {
@@ -19,5 +21,12 @@ class ProductType extends AbstractType
             ->add('price', NumberType::class)
             ->add('save', SubmitType::class)
         ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class
+        ]);
     }
 }
